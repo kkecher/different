cc8369233a412942a222d5aeb727bfc71a1168fc
kkec4er <kkecher@outlook.com>
2019-06-29 21:05:40 +1000

    New files

--- Python. Разносим заголовки по разделам в УКЛ.py
+++ Python. Разносим заголовки по разделам в УКЛ.py
@@ -15,8 +15,7 @@ import timeit
 start_time = timeit.default_timer()
 
 #загружаем список заголовков
-#subjects_file = input('Enter file with subjects: ')
-subjects_file = 'Масла трансмиссионные. ATF.txt'
+subjects_file = input('Enter file with subjects: ')
 with open(subjects_file, 'r', encoding='utf-8') as f:
     subjects_list = f.read().splitlines()
 
@@ -35,7 +34,7 @@ id_and_name_of_dirs_dict = {}
 for line in id_and_name_of_dirs_list:
     line_id_and_name_of_dirs_list = line.split(' — ')
     dir_id = line_id_and_name_of_dirs_list[0].strip()
-    dir_name = line_id_and_name_of_dirs_list[1].strip()
+    dir_name = line_id_and_name_of_dirs_list[1].strip().upper()
     id_and_name_of_dirs_dict[dir_id] = dir_name
 
 #создаем список с регулярками и id разделов
@@ -50,8 +49,8 @@ with open(ucl_file, 'r', encoding='utf-8') as f:
         line = re.sub(r'^\/', '', line, flags=re.IGNORECASE)  #убираем / с начала строки
         line = re.sub(r', "autoparts"', '', line, flags=re.IGNORECASE) #убираем "autoparts"
         line = re.sub(r", 'autoparts'", '', line, flags=re.IGNORECASE) #убираем 'autoparts'. Таких сейчас нет, но вдруг
-        line = re.sub(r'\/", ', ', ', line, flags=re.IGNORECASE) #убираем /", в конце
-        line = re.sub(r'", ', ', ', line, flags=re.IGNORECASE)  #убираем ", в конце
+        line = re.sub(r'\/",\s?', ', ', line, flags=re.IGNORECASE) #убираем /", в конце
+        line = re.sub(r'",\s?', ', ', line, flags=re.IGNORECASE)  #убираем ", в конце
 
         #если строка содержит регулярку, то обновляем словарь regex_dict; key=dir_id, value=кортеж (<регулярка>, <номер строки с регуляркой>):
         is_regex = re.search(r'(^.*),\s?(\d+$)', line, flags=re.IGNORECASE)
@@ -84,19 +83,18 @@ for dir_id in regex_dict:
     regex_compiled = re.compile(regex_string, flags=re.IGNORECASE)
     regex_compiled_dict[dir_id] = regex_compiled
 
-def is_match_regex_compiled(subject, dir_id):
+def is_match_regex_compiled(normalized_subject, dir_id):
     regex = regex_compiled_dict[dir_id]
-    is_match = re.search(regex, subject)
+    is_match = re.search(regex, normalized_subject)
     return(is_match)
 
-def collect_subjects_matched_to_dir_id_regexes(subject, dir_id):
+def collect_subjects_matched_to_dir_id_regexes(normalized_subject, dir_id):
     subject_regex_iter_dict = {}
     regex_list = regex_dict[dir_id]
     for regex_with_regex_iter_tuple in regex_list:
         regex = regex_with_regex_iter_tuple[0]
         regex_iter = regex_with_regex_iter_tuple[1]
-        #print(regex, regex_iter) #debug
-        if re.search(regex, subject, flags=re.IGNORECASE):
+        if re.search(regex, normalized_subject, flags=re.IGNORECASE):
             subject_regex_iter_dict.setdefault(subject, []).append(regex_iter)
     return(subject_regex_iter_dict)
 
@@ -106,17 +104,22 @@ subjects_match_more_than_1_id_dict = {} #создаем словарь; key=subj
 subject_iter = 1
 len_subjects_list = len(subjects_list)
 for subject in subjects_list:
+    #Нормируем заголовок, но в файл с результатами будем записываем исходный
+    normalized_subject = subject
+    normalized_subject = re.sub('(_|-|/|,|\s|[^\w\s/-])+', ' ', normalized_subject, flags=re.IGNORECASE)
+    normalized_subject = re.sub('^ | $', '', normalized_subject, flags=re.IGNORECASE)
+
     is_match_dir_id = False
     print('Doing ' + str(subject_iter) + ' of ' + str(len_subjects_list) + ' subjects')
     for dir_id in regex_dict:
-        if is_match_regex_compiled(subject, dir_id):
+        if is_match_regex_compiled(normalized_subject, dir_id):
             is_match_dir_id = True #если заголовок распознался регулярками раздела, то НЕ добавляем его потом в список «Нет соответствий»
-            subject_regex_iter_dict = collect_subjects_matched_to_dir_id_regexes(subject, dir_id)
+            subject_regex_iter_dict = collect_subjects_matched_to_dir_id_regexes(normalized_subject, dir_id)
             subjects_filtered_by_dirs_dict.setdefault(dir_id, {}).update(subject_regex_iter_dict)
             subjects_match_more_than_1_id_dict.setdefault(subject, []).append(dir_id) #сюда пока записываем ВСЕ id разделов, которым соответствует заголовок (даже если заголовок соответствует всего одному id). Потом оставлю уникальные значения и возьму заголовки, где число id>1. Не хочу вводить еще одно условие, и так хер разберешь уже:(
     if not is_match_dir_id:
         subject_regex_iter_dict = {subject: [None]}
-        subjects_filtered_by_dirs_dict.setdefault('Нет соответствий', {}).update(subject_regex_iter_dict)
+        subjects_filtered_by_dirs_dict.setdefault('НЕТ СООТВЕТСТВИЙ', {}).update(subject_regex_iter_dict)
     subject_iter+=1
 
 #записываем в файл полученные соответствия id-заголовок и название раздела
@@ -125,10 +128,10 @@ with open(result_file, 'w') as f:
         if dir_id in id_and_name_of_dirs_dict: #если id раздела есть в списке id-название, то получаем название раздела
             dir_name = id_and_name_of_dirs_dict[dir_id]
             f.write(str(dir_name) + ' (' + str(dir_id) + ')' + '\n')
-        elif dir_id != 'Нет соответствий': #если id нет в списке id-название и id НЕ равен «Нет соответствия», то пишем в файл предупреждение
+        elif dir_id != 'НЕТ СООТВЕТСТВИЙ': #если id нет в списке id-название и id НЕ равен «Нет соответствия», то пишем в файл предупреждение
             f.write('АХТУНГ! - НЕТ НАЗВАНИЯ РАЗДЕЛА' + ' (' + str(dir_id) + ')' + '\n')
         else:
-            f.write(dir_id + '\n') #dir_id = Нет соответствия
+            f.write(dir_id + '\n') #dir_id = НЕТ СООТВЕТСТВИЯ
         f.write('#'*50 + '\n')
         for subject, regex_iter_list in subject_regex_iter_dict.items():
             f.write(str(subject) + ' ### ')
@@ -145,7 +148,7 @@ with open(result_file, 'w') as f:
     f.write('\n'*5)
 
     #записываем заголовки, которые соответствуют более, чем одному id
-    f.write('Заголовки соответствуют более, чем одному id' + '\n')
+    f.write('ЗАГОЛОВКИ СООТВЕТСТВУЮТ БОЛЕЕ, ЧЕМ ОДНОМУ ID' + '\n')
     f.write('#'*50 + '\n')
     for subject, dir_id_list in subjects_match_more_than_1_id_dict.items():
             dir_id_set = set(dir_id_list) #оставляем уникальные id разделов
